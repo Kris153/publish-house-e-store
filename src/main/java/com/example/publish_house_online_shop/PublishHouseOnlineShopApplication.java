288f397f9f92c1c420310f358ade4004b4731b98
Kris <kris.nikolov@abv.bg>
2024-07-21 13:33:23 +0300

    First commit

---
+++ src/main/java/com/example/publish_house_online_shop/PublishHouseOnlineShopApplication.java
@@ -0,0 +1,13 @@
+package com.example.publish_house_online_shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PublishHouseOnlineShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PublishHouseOnlineShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/publish_house_online_shop/PublishHouseOnlineShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.publish_house_online_shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PublishHouseOnlineShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
