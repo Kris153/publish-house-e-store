f2a6beb9e13b16ee0ca9a0ddb42fd2fcb0cfc583
Kris153 <82515824+Kris153@users.noreply.github.com>
2024-07-27 18:48:54 +0300

    Add files via upload

---
+++ publish-house-books/src/main/java/org/example/publishhousebooks/PublishHouseBooksApplication.java
@@ -0,0 +1,13 @@
+package org.example.publishhousebooks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PublishHouseBooksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PublishHouseBooksApplication.class, args);
+    }
+
+}
